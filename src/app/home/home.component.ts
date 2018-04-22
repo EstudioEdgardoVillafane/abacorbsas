096fb974a5bc90621d20918f2817ff61d10ab2c9
damisabbatella <dsabbatella@yahoo.com.ar>
2018-04-22 09:18:59 -0300

    Realizacion validacion contacto home verificar si recibe email toto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,28 +15,69 @@ export class HomeComponent implements OnInit {
   phone;
   email;
   question;
+  BooleanToAlertNombre = false;
+  BooleanToAlertEmail = false;
+  BooleanToAlertTelefono = false;
+  BooleanToAlertMensaje = false;
+  BooleanToValidate = false;
   
+
   constructor( private contactoService: ContactoService) { }
 
   ngOnInit() {
   }
-  onSend(){
-    this.name = document.getElementById("name");
-    this.phone = document.getElementById("phone");
-    this.email = document.getElementById("email");
-    this.question = document.getElementById("question");
-    this.contactoService.Send(this.email.value,this.question.value,this.name.value,this.phone.value)
-    .subscribe((data) => {
-      console.log(data);
-      location.href="/enviado";
-    });
-  }
+ 
 
   onSelect(prod: Productos): void {
    // alert('hola' + prod.id );
     this.selectedProduct = prod;
 
    }
- 
+   ValidateForm(){
+    this.name = document.getElementById('name');
+    this.phone = document.getElementById('phone');
+    this.email = document.getElementById('email');
+    this.question = document.getElementById('question');
 
+
+    if(this.name.value == ""){
+      this.BooleanToAlertNombre = true;
+    }else{
+      this.BooleanToAlertNombre = false;
+    } 
+    if(this.phone.value == ""){
+      this.BooleanToAlertTelefono = true;
+    }else{
+      this.BooleanToAlertTelefono= false;
+    } 
+    if(this.email.value == ""){
+      this.BooleanToAlertEmail = true;
+    }else{
+      this.BooleanToAlertEmail = false;
+    }
+    if(this.question.value == ""){
+      this.BooleanToAlertMensaje = true;
+    }else{
+      this.BooleanToAlertMensaje = false;
+    }
+    if(this.name.value != "" && this.email.value != "" && this.phone.value !=""&& this.question.value !=""){
+      this.BooleanToValidate =true;
+    }
+    return this.BooleanToValidate;
+  
+  }
+  onSend(){
+
+    if(this.ValidateForm()){
+      this.name = document.getElementById("name");
+      this.phone = document.getElementById("phone");
+      this.email = document.getElementById("email");
+      this.question = document.getElementById("question");
+      this.contactoService.Send(this.email.value,this.question.value,this.name.value,this.phone.value)
+      .subscribe((data) => {
+        console.log(data);
+        location.href="/enviado";
+      });
+  }
+  }
 }
